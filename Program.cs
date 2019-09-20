eff46dc8267cb6e80fedd658e9f7f99ed84574a5
Maximiliam02 <54435275+Maximiliam02@users.noreply.github.com>
2019-09-20 09:50:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Cirkel_area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radie = 2.4;
+            Console.WriteLine("Cirkelns omrekts: "+ Omkrets(radie));
+            Console.WriteLine("Cirkelns area: "+ Area(radie));
+
+        }
+
+
+        static double Area(double r)
+        {
+            double a = r * r * Math.PI ;
+            return a;
+        }
+
+        static double Omkrets(double r)
+        {
+            double b = r * 2 * Math.PI;
+            return b;
+        }
+    }
+}
